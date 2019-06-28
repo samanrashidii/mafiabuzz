50173c1dc576bae46f82a4b56db519887afb51ae
Saman Rashidi <samanrashidi44@gmail.com>
2019-06-29 03:19:26 +0430

    fixed some issues

--- src/views/Creator.vue
+++ src/views/Creator.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="creator">
         <app-navigation />
-        <h2>Creator</h2>
+        <h2>Hi <span>God</span></h2>
+        <p>I know you are God but do you know how to start the game?<br /> okay if you don't, I'll tell you how :)</p>
     </div>
 </template>
 
