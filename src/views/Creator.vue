c513e629526681d4bcd83614fe89f0cc4ba63ef9
Saman Rashidi <samanrashidi44@gmail.com>
2019-06-30 04:21:12 +0430

    step box 3 created

--- src/views/Creator.vue
+++ src/views/Creator.vue
@@ -6,16 +6,26 @@
         <div class="steps">
             <form action="#" method="POST" accept-charset="utf-8" name="game_settings" class="game_settings"></form>
             <div class="step-box">
-                <label for="quantity"><span>1</span> Choose how many people do want to have in your world</label>
+                <span class="step-number">1</span>
+                <label for="quantity">Choose how many people do want to have in your universe:</label>
                 <select name="quantity" id="quantity" v-model="gameSettings.unit">
                     <option v-for="n in 25" :key="n">{{n+5}}</option>
                 </select>
             </div>
             <div class="step-box">
-                <label for="mafia_number"><span>2</span> Please choose how many of these people are mafia</label>
+                <span class="step-number">2</span>
+                <label for="mafia_number">Please choose how many of these people will be mafia:</label>
                 <select name="mafia_number" id="mafia_number" v-model="gameSettings.mafia">
-                    <option v-for="n in 12" :key="n">{{n+1}}</option>
+                    <option v-for="n in calcMafia" :key="n">{{n+1}}</option>
                 </select>
+            </div>
+            <div class="step-box">
+                <span class="step-number">3</span>
+                <label>Now you must choose between below roles for your game:</label>
+                <ul class="note">
+                    <li>Mafia roles have <span>red border</span> and you can choose maximum of <i>{{gameSettings.mafia}}</i> Mafia roles.</li>
+                    <li>Citizen roles have <strong>green border</strong> and you can choose maximum of <i>{{gameSettings.citizens}}</i> Citizen roles.</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -28,8 +38,16 @@ export default {
             gameSettings: {
                 unit: 6,
                 mafia: 2,
+                citizens: 4,
             }
         }
+    },
+    computed:{
+        calcMafia(){
+            let mafiaNumbers = Math.floor(this.gameSettings.unit / 3);
+            this.gameSettings.citizens = this.gameSettings.unit - this.gameSettings.mafia;
+            return mafiaNumbers-1;
+        },
     }
 }
 </script>
@@ -39,31 +57,49 @@ export default {
 .step-box{
     position: relative;
     padding:20px 15px;
-    margin-top:30px;
+    margin-top:40px;
     box-shadow: 0 0 10px $color_black;
 }
 
-.steps label span{
+span.step-number{
     position: absolute;
-    top:-16px;
-    left:-10px;
+    top:-20px;
+    left:-4px;
     display: inline-block;
-    width:30px;
-    height: 30px;
-    line-height: 30px;
+    width:34px;
+    height: 20px;
+    line-height: 20px;
+    font-size: $font_size_4;
     color: $color_2;
     text-align: center;
     margin-right: 4px;
     background-color:$creator_color;
-    border-radius: 50%;
+    border-radius: 1px;
 }
 
 .step-box label{
     display:inline-block;
     font-size: $font_size_4;
     color:$color_site_3;
-    margin-bottom:15px;
+    margin-bottom:20px;
 }
+
+.note li{
+    font-size: $font_size_3;
+    color:$color_4;
+    margin-bottom:12px;
+}
+
+.note li::before{
+    content:'*';
+    display: inline-block;
+    vertical-align: middle;
+    margin-right: 7px;
+}
+
+.note li strong,
+.note li i{color:$green_color;}
+.note li span{color:$red_color}
 
 </style>
 
