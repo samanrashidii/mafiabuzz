739353b63e35390ac0200a4f7dc52a82e064df11
Saman Rashidi <samanrashidi44@gmail.com>
2019-06-30 19:28:39 +0430

    working on creator page

---
+++ src/components/Roles.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="roles">
+        <ul class="has-clear-fix">
+            <li v-for="(role, index) in Roles" :key="index" :class="{'mafia': role.mafia}">
+                <input @change="checkRoles()" type="checkbox" name="roles" :id="`role_${index+1}`" :value="roleControl(role.name, role.mafia)" v-model="selectedRoles" :disabled="selectedMafia > selectedUnits.mafia" />
+                <label :for="`role_${index+1}`">
+                    <div class="inner-label">
+                        <img :src="getImgUrl(role.icon)" :alt="role.alt" />
+                        <strong>{{role.name}}</strong>
+                    </div>
+                </label>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+export default {
+    props:{
+        selectedUnits: {
+            type: Object
+        }
+    },
+    data(){
+        return {
+            selectedRoles: [],
+            selectedMafia: 0,
+            selectedCitizen: 0
+        }
+    },
+    computed:{
+        ...mapGetters([
+            'Roles',
+        ])
+    },
+    methods:{
+        getImgUrl(pic) {
+            return require(`@/assets/images/roles/${pic}`);
+        },
+        roleControl(name,role){
+            return {
+                name : name,
+                mafia : role
+            }
+        },
+        checkRoles(){
+            this.selectedMafia = this.selectedRoles.filter(x => x.mafia == true).length;
+            this.selectedCitizen = this.selectedRoles.filter(x => x.mafia == false).length;
+            this.$emit('selectedRoles', this.selectedRoles)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.roles{margin-top:15px;}
+.roles li{
+    float: left;
+    width:48%;
+    margin:4% 0 0 4%;
+    &:nth-child(2n+1){
+        margin-left:0;
+    }
+}
+
+.roles li label{
+    position: relative;
+    display: table;
+    width: 100%;
+    height: 140px;
+    font-family: $font_mafia;
+    font-size: 30px;
+    color:$color_1;
+    text-align: center;
+    padding:5px 10px;
+    cursor: pointer;
+    background-color: $background_color_citizen;
+    border:3px solid $black_color;
+    border-radius: 2px;
+    > div{
+        display: table-cell;
+        vertical-align: middle;
+        strong{
+            display: block;
+            margin-top:5px;
+        }
+    }
+}
+
+.roles li.mafia label{background-color:$background_color_mafia;}
+
+.roles li input{display: none;}
+
+
+
+</style>
