e64a13682e64e2648d87e104c45624f583f49744
Saman Rashidi <samanrashidi44@gmail.com>
2019-07-03 00:16:34 +0430

    overlay created and errors has been finished for role validation

---
+++ src/components/global/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <a class="bttn" href="javascript:void(0)">
+         <slot></slot>
+    </a>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+a.bttn{
+    display:block;
+    color:$color_1;
+    text-align: center;
+    padding:14px;
+    margin-top:15px;
+    background-color:$background_color_3;
+    border-radius: 2px;
+    transition:all .2s ease-in-out;
+    &:hover{
+        background-color: $green_color;
+    }
+}
+
+</style>
+
