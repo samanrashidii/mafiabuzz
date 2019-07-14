a135c53f36bb9379c0d472dc5dd82d239afa4b32
Saman Rashidi <samanrashidi44@gmail.com>
2019-07-14 16:20:56 +0430

    all dummy data entried and fixed validation issues for under construction pages

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -2,80 +2,94 @@
     <div class="create">
         <div class="dashboard-header">
             <div>
-                <router-link class="site-bttn game-mode" :to="{name : 'home'}"><span>Change Game Mode</span></router-link>
+                <router-link class="site-bttn game-mode" :to="{name : 'home'}"><span>{{Creator.gameModeButton}}</span></router-link>
             </div>
             <div class="title">
-                <h2 v-html="Creator.title"></h2>
-                <p v-html="Creator.subtitle"></p>
+                <template v-if="checkGameMode()">
+                    <h2 v-html="Creator.title"></h2>
+                    <p v-html="Creator.subtitle"></p>
+                </template>
+                <!-- Under Construction -->
+                <template v-else>
+                    <img :src="require(`@/assets/images/under-construction.png`)" alt="Under Construction Icon" />
+                    <h2>We are <span>Sorry</span></h2>
+                    <p>This mode is under construction ...!!!</p>
+                </template>
             </div>
         </div>
-        <form action="#" method="POST" accept-charset="utf-8" name="game_settings" class="game_settings">
-            <div class="steps">
-                <div class="step-box">
-                    <span class="step-number">1</span>
-                    <label for="quantity">{{Creator.step1}}</label>
-                    <select name="quantity" id="quantity" v-model="gameSettings.unit">
-                        <option v-for="n in Creator.maxPlayers" :key="n">{{n + Creator.playerMargin}}</option>
-                    </select>
-                </div>
-                <div class="step-box">
-                    <span class="step-number">2</span>
-                    <label for="mafia_number">{{Creator.step2}}</label>
-                    <select name="mafia_number" id="mafia_number" v-model.number="gameSettings.mafia">
-                        <option v-for="n in calcMafia" :key="n">{{n+1}}</option>
-                    </select>
-                </div>
-                <div class="step-box">
-                    <span class="step-number">3</span>
-                    <label>{{Creator.step3}}</label>
-                    <ul class="note">
-                        <li>Mafia characters have <span class="mafia-role">red color</span> and you can choose <i class="mafia-role">{{gameSettings.mafia}}</i> of them.</li>
-                        <li>Citizen characters have <strong class="citizen-role">blue color</strong> and you can choose <i class="citizen-role">{{gameSettings.citizens}}</i> of them.</li>
-                    </ul>
-                </div>
-            </div>
-            <roles @selectedRoles="gameSettings.roles = $event"></roles>
-            <app-button @click.native="checkGame()" class="active start-bttn"><span>{{Creator.start}}</span></app-button>
-            <overlay :class="{'active': overlay,'dialog': isValid}">
-                <template v-if="isValid">
-                    <img class="has-bottom-margin" :src="require(`@/assets/images/icons/not-valid.png`)" alt="Not Valid Icon" />
-                    <ul class="error-bullet">
-                        <li v-if="error.mafia">
-                            You have chosen <span>{{gameSettings.mafia}}</span> Mafia characters but selected <i class="mafia-role">{{gameValdiation.selectedMafia}}</i>
-                        </li>
-                        <li class="blue" v-if="error.citizens">
-                            You have chosen <span>{{gameSettings.citizens}}</span> Citizen characters but selected <i class="citizen-role">{{gameValdiation.selectedCitizen}}</i>
-                        </li>
-                    </ul>
-                    <app-button @click.native="overlay = false" class="settings-bttn"><span>Change Role Settings</span></app-button>
-                </template>
-                <template v-else>
-                    <div class="note-box">
-                        <img class="has-xsmall-bottom-margin" :src="require(`@/assets/images/icons/info.png`)" alt="Info Icon" />
-                        <h3>Your game will start with below characters</h3>
+        <template v-if="checkGameMode()">
+            <form action="#" method="POST" accept-charset="utf-8" name="game_settings" class="game_settings">
+                <div class="steps">
+                    <div class="step-box">
+                        <span class="step-number">1</span>
+                        <label for="quantity">{{Creator.step1}}</label>
+                        <select name="quantity" id="quantity" v-model="gameSettings.unit">
+                            <option v-for="n in Creator.maxPlayers" :key="n">{{n + Creator.playerMargin}}</option>
+                        </select>
                     </div>
-                    <div class="table mafia-table">
-                        <table>
-                            <tr v-for="(fM, index) in finalMafias" :key="index">
-                                <td><img :src="getImgUrl(fM.icon)" :alt="fM.alt" /> {{fM.name}}</td>
-                                <td><span class="character-power">{{Math.abs(fM.power)}}</span></td>
-                            </tr>
-                        </table>
+                    <div class="step-box">
+                        <span class="step-number">2</span>
+                        <label for="mafia_number">{{Creator.step2}}</label>
+                        <select name="mafia_number" id="mafia_number" v-model.number="gameSettings.mafia">
+                            <option v-for="n in calcMafia" :key="n">{{n+1}}</option>
+                        </select>
                     </div>
-                    <div class="table citizen-table">
-                        <table>
-                            <tr v-for="(fC, index) in finalCitizens" :key="index">
-                                <td><img :src="getImgUrl(fC.icon)" :alt="fC.alt" /> {{fC.name}}</td>
-                                <td><span class="character-power">{{Math.abs(fC.power)}}</span></td>
-                            </tr>
-                        </table>
+                    <div class="step-box">
+                        <span class="step-number">3</span>
+                        <label>{{Creator.step3}}</label>
+                        <ul class="note">
+                            <li>Mafia characters have <span class="mafia-role">red color</span> and you can choose <i class="mafia-role">{{gameSettings.mafia}}</i> of them.</li>
+                            <li>Citizen characters have <strong class="citizen-role">blue color</strong> and you can choose <i class="citizen-role">{{gameSettings.citizens}}</i> of them.</li>
+                        </ul>
                     </div>
-                    <app-button @click.native="overlay = false" class="settings-bttn"><span>Change Role Settings</span></app-button>
-                    <app-button @click.native="startGame()" class="active start-bttn"><span>Start Game!</span></app-button>
-                </template>
-            </overlay>
-        </form>
-        <power-meter :power="calcPower" :mafia="calcDifference(gameSettings.mafia, gameValdiation.selectedMafia)" :citizen="calcDifference(gameSettings.citizens, gameValdiation.selectedCitizen)" :class="{'active': !isValid}"></power-meter>
+                </div>
+                
+                <roles @selectedRoles="gameSettings.roles = $event"></roles>
+
+                <app-button @click.native="checkGame()" class="active start-bttn"><span>{{Creator.start}}</span></app-button>
+
+                <overlay :class="{'active': overlay,'dialog': isValid}">
+                    <template v-if="isValid">
+                        <img class="has-bottom-margin" :src="require(`@/assets/images/icons/not-valid.png`)" alt="Not Valid Icon" />
+                        <ul class="error-bullet">
+                            <li v-if="error.mafia">
+                                You have chosen <span>{{gameSettings.mafia}}</span> Mafia characters but selected <i class="mafia-role">{{gameValdiation.selectedMafia}}</i>
+                            </li>
+                            <li class="blue" v-if="error.citizens">
+                                You have chosen <span>{{gameSettings.citizens}}</span> Citizen characters but selected <i class="citizen-role">{{gameValdiation.selectedCitizen}}</i>
+                            </li>
+                        </ul>
+                        <app-button @click.native="overlay = false" class="settings-bttn"><span>{{Creator.changeSettings}}</span></app-button>
+                    </template>
+                    <template v-else>
+                        <div class="note-box">
+                            <img class="has-xsmall-bottom-margin" :src="require(`@/assets/images/icons/info.png`)" alt="Info Icon" />
+                            <h3>{{Creator.checkBeforeStart}}</h3>
+                        </div>
+                        <div class="table mafia-table">
+                            <table>
+                                <tr v-for="(fM, index) in finalMafias" :key="index">
+                                    <td><img :src="getImgUrl(fM.icon)" :alt="fM.alt" /> {{fM.name}}</td>
+                                    <td><span class="character-power">{{Math.abs(fM.power)}}</span></td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="table citizen-table">
+                            <table>
+                                <tr v-for="(fC, index) in finalCitizens" :key="index">
+                                    <td><img :src="getImgUrl(fC.icon)" :alt="fC.alt" /> {{fC.name}}</td>
+                                    <td><span class="character-power">{{Math.abs(fC.power)}}</span></td>
+                                </tr>
+                            </table>
+                        </div>
+                        <app-button @click.native="overlay = false" class="settings-bttn"><span>{{Creator.changeSettings}}</span></app-button>
+                        <app-button @click.native="startGame()" class="active start-bttn"><span>{{Creator.start}}</span></app-button>
+                    </template>
+                </overlay>
+                
+            </form>
+            <power-meter :power="calcPower" :mafia="calcDifference(gameSettings.mafia, gameValdiation.selectedMafia)" :citizen="calcDifference(gameSettings.citizens, gameValdiation.selectedCitizen)" :class="{'active': !isValid}"></power-meter>
+        </template>
     </div>
 </template>
 
@@ -168,6 +182,11 @@ export default {
             'setGame',
             'setNumbers',
         ]),
+        checkGameMode(){
+            if(this.$route.params.id == 'single-device'){
+                return true;
+            }
+        },
         getImgUrl(pic) {
             return require(`@/assets/images/roles/${pic}`);
         },
