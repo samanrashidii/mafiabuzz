96521a08fb29d879efe498d46b74c21bef7fcd01
Saman Rashidi <samanrashidi44@gmail.com>
2019-07-18 00:29:37 +0430

    ruspy character now can silence himself or his teammates

--- src/components/God.vue
+++ src/components/God.vue
@@ -264,8 +264,8 @@ export default {
             else if(player.id == 7){
                return this.finalPlayers.filter(x => x.mafia != player.mafia && x.status.dead == false);
             }
-            // Doctor Target
-            else if(player.id == 10){
+            // Doctor & Ruspy Target
+            else if(player.id == 10 || player.id == 3){
                return this.finalPlayers.filter(x => x.status.dead == false);
             }
             // Default Target
