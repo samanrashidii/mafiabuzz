4fa7fc77673d0a9acc791e3a41ce0ffa62e520a8
Samanrashidii <samanrashidi44@gmail.com>
2022-07-01 15:16:17 +0430

    update: characters json to v2.1

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 const { BASE_URL } = process.env
 export default {
   // Characters
-  getRoles: { method: 'GET', url: `${BASE_URL}/api/characters.v2.0.json` },
+  getRoles: { method: 'GET', url: `${BASE_URL}/api/characters.v2.1.json` },
   getReplacingRoles: { method: 'GET', url: `${BASE_URL}/api/replacingRoles.json` },
 
   // Third Party
