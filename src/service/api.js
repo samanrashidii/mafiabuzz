e3b5404e9f41e35d93b5c3330422df35d2928d9a
Samanrashidii <samanrashidi44@gmail.com>
2022-07-01 04:54:18 +0430

    fix: characters cache issue

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 const { BASE_URL } = process.env
 export default {
   // Characters
-  getRoles: { method: 'GET', url: `${BASE_URL}/api/characters.json` },
+  getRoles: { method: 'GET', url: `${BASE_URL}/api/characters.v2.0.json` },
   getReplacingRoles: { method: 'GET', url: `${BASE_URL}/api/replacingRoles.json` },
 
   // Third Party
