c8ed42ee69444af7def722a51a0113cb6313e0eb
Samanrashidii <samanrashidi44@gmail.com>
2023-02-10 16:17:02 +0330

    fix: show dead persons on action bar when they have used or not used action

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 const { BASE_URL } = process.env
 export default {
   // Characters
-  getRoles: { method: 'GET', url: `${BASE_URL}/api/characters.v2.3.json` },
+  getRoles: { method: 'GET', url: `${BASE_URL}/api/characters.v2.4.json` },
   getReplacingRoles: { method: 'GET', url: `${BASE_URL}/api/replacingRoles.v2.4.json` },
   // Third Party
   postToDiscord: { method: 'POST' }
